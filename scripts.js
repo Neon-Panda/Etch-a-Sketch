ef4b2b187766bedec1b985446f8f74bbb312f48e
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-25 05:10:53 -0600

    add opacity feature

--- scripts.js
+++ scripts.js
@@ -9,16 +9,30 @@ function createSquares() {
         const rowDiv = document.createElement("div");
         rowDiv.classList.add("square");
         rowDiv.style.height = `${800 / numberOfSquares}px`;
-        rowDiv.style.width = `${800 / numberOfSquares}px`
+        rowDiv.style.width = `${800 / numberOfSquares}px`;
         container.appendChild(rowDiv)}}
 
         const squareDiv = document.querySelectorAll(".square");
         squareDiv.forEach( (square) => {
-        square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "green";
+            let opacity = 0.1;
+            square.addEventListener("mouseenter", () => {
+                if (square.style.backgroundColor == "") {
+                    square.style.backgroundColor = randomColor();
+                }
+                if (parseInt(square.style.opacity) < 1)
+                    opacity += 0.2;
+                    square.style.opacity = opacity.toString();
     })
 })}
 createSquares()
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const rgb = `rgb(${r}, ${g}, ${b})`
+    return rgb
+}
 
 function deleteSquares() {
     const squareDiv = document.querySelectorAll(".square");
