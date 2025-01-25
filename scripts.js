32a229ccfaaa1ce84473254aab77085fe5990137
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-25 04:02:26 -0600

    add square re-sizing

--- scripts.js
+++ scripts.js
@@ -11,19 +11,17 @@ function createSquares() {
         rowDiv.style.height = `${800 / numberOfSquares}px`;
         rowDiv.style.width = `${800 / numberOfSquares}px`
         container.appendChild(rowDiv)}}
-        const squareDiv = document.querySelectorAll(".square");
 
+        const squareDiv = document.querySelectorAll(".square");
         squareDiv.forEach( (square) => {
         square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
+        square.style.backgroundColor = "green";
     })
 })}
-
 createSquares()
 
 function deleteSquares() {
     const squareDiv = document.querySelectorAll(".square");
-
     squareDiv.forEach( (square) => {
         square.remove();
     })
@@ -34,8 +32,10 @@ function deleteSquares() {
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    numberOfSquares = prompt("How many rows & columns should the new grid have?")
-    deleteSquares()
-    createSquares()
-    addEventsToSquares()
+    numberSubmission = prompt("How many rows & columns should the new grid have? Must be less than 100")
+    if (numberSubmission < 100 || numberOfSquares > 0) {
+    numberOfSquares = numberSubmission
+}
+deleteSquares()
+createSquares()
 })
